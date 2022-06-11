aeb0cab51772688e0fbf66fe561f728715dc0363
Victoria LeBel <vickileb@hotmail.com>
2022-06-10 18:21:44 -0700

    commit tree d8014c284f6220705a3aeeccb25176c2cc8b6f13

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching (drivers, name){
+    const driverNames = drivers.filter(driver => driver.toUpperCase() === name.toUpperCase())
+    return driverNames
+}
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function fuzzyMatch(drivers, letters){
+    const driverFirstTwoLetters = drivers.filter(driver => driver.slice(0,2) === letters)
+    return driverFirstTwoLetters
+}
+
+function matchName(drivers, firstname){
+    const nameValue = drivers.filter(driver => driver.name === firstname)
+    return nameValue
+}
+
